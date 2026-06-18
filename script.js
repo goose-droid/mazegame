eb5699885a61942e6edbecffb3c654b03968e472
Ollie Christensen <ollie.a.c@outlook.com>
2026-06-18 13:15:58 -0600

    fixed maze arrays, added location of keys and treasure box to consts

--- script.js
+++ script.js
@@ -38,17 +38,26 @@ const ButtonWest = document.querySelector("#ButtonWest");
 
 //function for assigning next rooms*/
 
-//types of rooms (4 directions where each may be passable or not)
-const roomTypes = ["0000", "1111", "1000", "0100", "0010", "0001", "1100", "0110", "0011", "1001", "0101", "1010", "1110", "0111", "1011", "1101"]
+//types of rooms (4 directions where each may be passable or not, order NSEW)
+const roomTypes = ["0000", "1111", "1000", "0100", "0010", "0001", "1100", "0110", "0011", "1001", "0101", "1010", "1110", "0111", "1011", "1101"];
 
-//the "maze"
+//the "maze". the index of the outer array is 7. the index of each inner array is x. mirrored up/down visually compared to maze as drawn to avoid negatives in y axis. the content of the inner arrays is the room type, which refers to the index of the roomTypes array.
 const rooms = [
-    [0, 7, 8, 8, 13, 8, 8, 10],
+    [0, 11, 8, 8, 12, 8, 8, 9],
     [0, 6, 0, 0, 6, 0, 0, 6],
     [0, 6, 0, 0, 6, 0, 0, 6],
     [5, 1, 8, 8, 1, 8, 8, 15],
     [0, 6, 0, 0, 6, 0, 0, 6],
     [0, 6, 0, 0, 6, 0, 0, 6],
-    [0, 11, 8, 8, 12, 8, 8, 9]
-]
+    [0, 7, 8, 8, 13, 8, 8, 10]
+];
+
+//location of keys. the inner arrays are in format [x, y] for location on grid
+const keys = [
+    [3, 0],
+    [5, 6]
+];
+
+//location of treasure box. format [x,y]
+const treasureBox = [7, 3];
 
