e815414ed539f1b7a06b4a42a86a6fae8d6e79c2
Ollie Christensen <ollie.a.c@outlook.com>
2026-06-18 13:04:03 -0600

    add a bunch of arrays for room types and maze

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //room class
-class Room {
+/*class Room {
 
     //fields
     roomId;
@@ -36,4 +36,19 @@ const ButtonSouth = document.querySelector("#ButtonSouth");
 const ButtonEast = document.querySelector("#ButtonEast");
 const ButtonWest = document.querySelector("#ButtonWest");
 
-//function for assigning next rooms
+//function for assigning next rooms*/
+
+//types of rooms (4 directions where each may be passable or not)
+const roomTypes = ["0000", "1111", "1000", "0100", "0010", "0001", "1100", "0110", "0011", "1001", "0101", "1010", "1110", "0111", "1011", "1101"]
+
+//the "maze"
+const rooms = [
+    [0, 7, 8, 8, 13, 8, 8, 10],
+    [0, 6, 0, 0, 6, 0, 0, 6],
+    [0, 6, 0, 0, 6, 0, 0, 6],
+    [5, 1, 8, 8, 1, 8, 8, 15],
+    [0, 6, 0, 0, 6, 0, 0, 6],
+    [0, 6, 0, 0, 6, 0, 0, 6],
+    [0, 11, 8, 8, 12, 8, 8, 9]
+]
+
